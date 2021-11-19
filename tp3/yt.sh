9e6570e87e6c1a1b932f7a875fe18ec95554240f
AFERREIRA33 <alex.ferreira@ynov.com>
2021-11-19 11:28:47 +0100

    TP3-yt

--- tp3/yt.sh
+++ tp3/yt.sh
@@ -6,7 +6,7 @@ if [[ (-d "$dl") && (-d "$log") ]]; then
 	mkdir "/srv/yt/downloads/$title"
 	mkdir "/srv/yt/downloads/$title/description"
 	echo "Video $url was downloaded."
-        echo "File path : /srv/yt/downloads/$title/$title.mp4"
+    echo "File path : /srv/yt/downloads/$title/$title.mp4"
 	youtube-dl -o "/srv/yt/downloads/$title/%(title)s.%(ext)s" "$url" > "/dev/null"
 	youtube-dl -o "/srv/yt/downloads/$title/description/$title" --write-description --skip-download --youtube-skip-dash-manifest "https://www.youtube.com/watch?v=G_1IyYRdLBA" > "/dev/null"
 	echo "[$(date "+%Y/%m/%d %T")] Video $url was downloaded. File path : /srv/yt/downloads/$title/$title.mp4" >> "/var/log/yt/download.log"
